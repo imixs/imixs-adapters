3596b23c18fed1b999bd76bc192bf006feffc75c
Ralph Soika <ralph.soika@imixs.com>
2014-08-20 08:41:39 +0200

    issue#3

--- imixs-adapters-magento-ejb/src/main/java/org/imixs/workflow/magento/MagentoCache.java
+++ imixs-adapters-magento-ejb/src/main/java/org/imixs/workflow/magento/MagentoCache.java
@@ -27,7 +27,10 @@
 
 package org.imixs.workflow.magento;
 
+import java.util.Date;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.security.DeclareRoles;
@@ -40,10 +43,15 @@ import org.imixs.workflow.ItemCollection;
 
 /**
  * The MagentoCache is a sigelton EJB providing an application wide cache
- * mechanism to cache products and customer data
+ * mechanism to cache products and customer data.
  * 
+ * The property 'refreshtime' defines the max time a data will be cached. There
+ * for the bean stores the time of caching in the property $cached of an entiy.
  * 
- * @see http
+ * The method flush() will remove all entities where the $cached timestamp is
+ * older then the refresh time frame.
+ * 
+ * @see http 
  *      ://www.adam-bien.com/roller/abien/entry/singleton_the_perfect_cache_facade
  * @author rsoika
  */
@@ -64,19 +72,35 @@ public class MagentoCache {
 
 	private ConcurrentHashMap<String, ItemCollection> customerCache = null;
 	private ConcurrentHashMap<String, ItemCollection> productCache = null;
+	private int refresh = 600; // default 10 minutes
+
+	private static Logger logger = Logger.getLogger(MagentoCache.class
+			.getName());
 
 	@PostConstruct
 	public void initialize() {
 		clearCache();
 	}
 
-	
+	/**
+	 * get refresh time in seconds
+	 * 
+	 * @return
+	 */
+	public int getRefresh() {
+		return refresh;
+	}
+
+	public void setRefresh(int refresh) {
+		this.refresh = refresh;
+	}
+
 	public void clearCache() {
 		this.customerCache = new ConcurrentHashMap<String, ItemCollection>();
 		this.productCache = new ConcurrentHashMap<String, ItemCollection>();
-		
+
 	}
-	
+
 	/**
 	 * Returns a product from the cache
 	 * 
@@ -84,11 +108,9 @@ public class MagentoCache {
 	 * @return
 	 */
 	public ItemCollection getProduct(String id) {
-
-		return productCache.get(id);
-
+		return getEntity(productCache,id);
 	}
-	
+
 	/**
 	 * Returns a customer from the cache
 	 * 
@@ -97,18 +119,94 @@ public class MagentoCache {
 	 */
 	public ItemCollection getCustomer(String id) {
 
-		return customerCache.get(id);
+		return getEntity(customerCache,id);
 
 	}
-	
-	
-	public void cacheProduct(String id,ItemCollection product) {
+
+	/**
+	 * puts an entity into the cache and adds the property $cached with the
+	 * current timestamp
+	 * 
+	 * @param id
+	 * @param product
+	 */
+	public void cacheProduct(String id, ItemCollection product) {
+		// add the cached timestamp
+		product.replaceItemValue("$cached", new Date());
 		productCache.put(id, product);
 	}
-	
-	
-	public void cacheCustomer(String id,ItemCollection customer) {
+
+	/**
+	 * puts an entity into the cache and adds the property $cached with the
+	 * current timestamp
+	 * 
+	 * @param id
+	 * @param customer
+	 */
+	public void cacheCustomer(String id, ItemCollection customer) {
+		// add the cached timestamp
+		customer.replaceItemValue("$cached", new Date());
 		customerCache.put(id, customer);
+	}
+
+	/**
+	 * This method removes all entities form the cache where the $cached
+	 * timestamp is older then the refreshTime.
+	 */
+	public void flush() {
+		logger.info("[MagentoCache] flush cache...");
+		// check products....
+		flushCache(productCache);
+
+		// check customers....
+		flushCache(customerCache);
+	}
+
+	/**
+	 * gets an entity from a cache. The method compares the $cached timestamp
+	 * with the refreshtime. If the entity is deprecated it will be removed.
+	 * Then the method returns null.
+	 * 
+	 * @param key
+	 */
+	private ItemCollection getEntity(
+			ConcurrentHashMap<String, ItemCollection> cache, String key) {
+		Date now = new Date();
+		ItemCollection entity = cache.get(key);
+		// check date
+		if (entity != null) {
+			Date cached = entity.getItemValueDate("$cached");
+			if (cached == null
+					|| ((now.getTime() - cached.getTime()) / 1000) < refresh) {
+				logger.fine("[MagentoCache] deprecated entity: '" + key + "' will be remvoed");
+				cache.remove(key);
+				entity=null;
+			}
+		}
+		
+	
+		return entity;
+	}
+
+	/**
+	 * This method removes all entities form the cache where the $cached
+	 * timestamp is older then the refreshTime.
+	 */
+	private void flushCache(ConcurrentHashMap<String, ItemCollection> cache) {
+		Date now = new Date();
+		// check products....
+		Iterator<String> iter = cache.keySet().iterator();
+		while (iter.hasNext()) {
+			String key = iter.next().toString();
+			ItemCollection entity = cache.get(key);
+			Date cached = entity.getItemValueDate("$cached");
+			if (cached == null
+					|| ((now.getTime() - cached.getTime()) / 1000) < refresh) {
+				logger.fine("[MagentoCache] flush: '" + key+"'");
+				iter.remove();
+			}
+		}
+
 	}
 
 }
