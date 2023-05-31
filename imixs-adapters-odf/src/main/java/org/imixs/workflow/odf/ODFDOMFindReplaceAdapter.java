dd6903acfc80a14ac16dfda46c2717423813fd45
Ralph Soika <ralph.soika@imixs.com>
2023-05-31 18:56:53 +0200

    redesign api
    Issue #135

--- imixs-adapters-odf/src/main/java/org/imixs/workflow/odf/ODFDOMFindReplaceAdapter.java
+++ imixs-adapters-odf/src/main/java/org/imixs/workflow/odf/ODFDOMFindReplaceAdapter.java
@@ -18,7 +18,10 @@ import org.imixs.workflow.exceptions.AdapterException;
 import org.imixs.workflow.exceptions.PluginException;
 import org.imixs.workflow.util.XMLParser;
 import org.odftoolkit.odfdom.doc.OdfDocument;
+import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
 import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.odftoolkit.odfdom.doc.table.OdfTable;
+import org.odftoolkit.odfdom.doc.table.OdfTableCell;
 import org.odftoolkit.odfdom.incubator.search.InvalidNavigationException;
 import org.odftoolkit.odfdom.incubator.search.TextNavigation;
 import org.odftoolkit.odfdom.incubator.search.TextSelection;
@@ -79,17 +82,17 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
-	public ItemCollection execute(ItemCollection document, ItemCollection event)
+	public ItemCollection execute(ItemCollection workitem, ItemCollection event)
 			throws AdapterException, PluginException {
 
 		// read the config
-		ItemCollection odfConfig = workflowService.evalWorkflowResult(event, "odf-update", document, false);
+		ItemCollection odfConfig = workflowService.evalWorkflowResult(event, "odf-update", workitem, false);
 		if (odfConfig == null || !odfConfig.hasItem("findreplace")) {
 			throw new PluginException(ODFDOMFindReplaceAdapter.class.getSimpleName(), CONFIG_ERROR,
 					"Missing odf configuration");
 		}
 		String fileName = odfConfig.getItemValueString("filename");
-		fileName = workflowService.adaptText(fileName, document);
+		fileName = workflowService.adaptText(fileName, workitem);
 
 		if (!fileName.toLowerCase().endsWith(".odt") && !fileName.toLowerCase().endsWith(".ods")) {
 			throw new PluginException(ODFDOMFindReplaceAdapter.class.getSimpleName(), CONFIG_ERROR,
@@ -107,25 +110,25 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 		// First we test if the fileName is unique. If not found we test regular
 		// expressions...
 		boolean foundFile = false;
-		FileData fileData = document.getFileData(fileName);
+		FileData fileData = workitem.getFileData(fileName);
 		if (fileData != null) {
 			// file data found - so we can updated it....
 			foundFile = true;
-			updateFileData(fileData, document, replaceDevList, eval);
+			updateFileData(fileData, workitem, replaceDevList, eval);
 		} else {
 			// not found, we can test regular expressions...
-			List<String> fileNames = document.getFileNames();
+			List<String> fileNames = workitem.getFileNames();
 			Pattern pattern = Pattern.compile(fileName);
 			// get all fileNames....
 			for (String aFileName : fileNames) {
 				// test if aFilename matches the pattern or the pattern is null
 				if (pattern.matcher(aFileName).find()) {
 					// fetch the file
-					fileData = document.getFileData(aFileName);
+					fileData = workitem.getFileData(aFileName);
 					if (fileData != null) {
 						// file data found - so we can updated it....
 						foundFile = true;
-						updateFileData(fileData, document, replaceDevList, eval);
+						updateFileData(fileData, workitem, replaceDevList, eval);
 					}
 
 				}
@@ -138,7 +141,7 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 					"wrong odf configuration - no file found matching '" + fileName + "' !");
 		}
 
-		return document;
+		return workitem;
 	}
 
 	/**
@@ -157,7 +160,7 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 	 * @throws PluginException
 	 * 
 	 */
-	public void updateFileData(FileData fileData, ItemCollection document, List<String> replaceDevList, String eval)
+	public void updateFileData(FileData fileData, ItemCollection workitem, List<String> replaceDevList, String eval)
 			throws PluginException {
 		byte[] newContent = null;
 		
@@ -166,35 +169,26 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 		String fileName = fileData.getName();
 		if (fileData.getContent() == null || fileData.getContent().length < 3) {
 			// load the snapshot
-			fileData = snapshotService.getWorkItemFile(document.getUniqueID(), fileName);
+			fileData = snapshotService.getWorkItemFile(workitem.getUniqueID(), fileName);
 		}
 
-		// odf files....
-		if (fileName.toLowerCase().endsWith(".odt")) {
+		// odt files....
+		if (fileName.toLowerCase().endsWith(".odt")  || fileName.toLowerCase().endsWith(".ods")) {
 
 			try (InputStream inputStream = new ByteArrayInputStream(fileData.getContent())) {
+				OdfDocument odfDoc = OdfDocument.loadDocument(inputStream);
 
-				OdfTextDocument doc = (OdfTextDocument) OdfDocument.loadDocument(inputStream);
+			
+				logger.fine("OdfTextDocument loaded");
+				updateODFDocument(odfDoc,workitem,replaceDevList);
 
-				logger.fine("OdfDocument loaded");
-
-				for (String entityDev : replaceDevList) {
-					ItemCollection entityData = XMLParser.parseItemStructure(entityDev);
-
-					if (entityData != null) {
-						String find = entityData.getItemValueString("find");
-						find = workflowService.adaptText(find, document);
-						String replace = entityData.getItemValueString("replace");
-						replace = workflowService.adaptText(replace, document);
-						replaceODFDocument(doc, find, replace);
-					}
-				}
+			
 
 				logger.fine("findreplace completed");
 
 				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-				doc.save(byteArrayOutputStream);
-				doc.close();
+				odfDoc.save(byteArrayOutputStream);
+				odfDoc.close();
 				newContent = byteArrayOutputStream.toByteArray();
 
 			} catch (Exception e) {
@@ -205,17 +199,54 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 
 			FileData fileDataNew = new FileData(fileData.getName(), newContent, fileData.getContentType(), null);
 			// update the fileData
-			document.addFileData(fileDataNew);
+			workitem.addFileData(fileDataNew);
 			logger.fine("new document added");
 		}
+
+
+
+		
 	}
+
+	/**
+	 * Updates a Text Document
+	 * @param odfDoc
+	 * @param workitem
+	 * @param replaceDevList
+	 * @throws PluginException
+	 * @throws InvalidNavigationException
+	 */
+	public void updateODFDocument(OdfDocument odfDoc, ItemCollection workitem, List<String> replaceDevList) throws PluginException, InvalidNavigationException {
+		logger.fine("OdfTextDocument loaded");
+
+		for (String entityDev : replaceDevList) {
+			ItemCollection entityData = XMLParser.parseItemStructure(entityDev);
+
+			if (entityData != null) {
+				String find = entityData.getItemValueString("find");
+				find = workflowService.adaptText(find, workitem);
+				String replace = entityData.getItemValueString("replace");
+				replace = workflowService.adaptText(replace, workitem);
+
+				if (odfDoc instanceof OdfTextDocument) {
+					replaceODFTextFragment((OdfTextDocument) odfDoc, find, replace);
+				}
+				if (odfDoc instanceof OdfSpreadsheetDocument) {
+					replaceODFCell((OdfSpreadsheetDocument) odfDoc, find, replace);
+				}
+				
+			}
+		}
+	}
+
+
 
 	/**
 	 * Helper method replaces a given text in a OdfTextDocument
 	 * 
 	 * @throws InvalidNavigationException
 	 */
-	private void replaceODFDocument(OdfTextDocument doc, String pattern, String replace)
+	private void replaceODFTextFragment(OdfTextDocument doc, String pattern, String replace)
 			throws InvalidNavigationException {
 
 		TextNavigation searchPattern = new TextNavigation(pattern, doc);
@@ -224,6 +255,22 @@ public class ODFDOMFindReplaceAdapter implements SignalAdapter {
 			TextSelection textSelection = (TextSelection) searchPattern.getCurrentItem();
 			textSelection.replaceWith(replace);
 		}
+
+	}
+
+
+	/**
+	 * Helper method replaces a given text in a OdfTextDocument
+	 * 
+	 * @throws InvalidNavigationException
+	 */
+	private void replaceODFCell(OdfSpreadsheetDocument doc, String address, String replace)
+			throws InvalidNavigationException {
+
+		// get first table sheet...
+		OdfTable tbl = doc.getTableList().get(0);
+		OdfTableCell cell = tbl.getCellByPosition(address);
+		cell.setStringValue(replace);
 
 	}
 
